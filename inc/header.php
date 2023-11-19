0abd3a98073c79409ae5f0aeb92bea8d67d3cb5f
EbrahiM <ebrahim49119782@gmail.com>
2023-11-20 00:50:51 +0600

    header add

--- inc/header.php
+++ inc/header.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Create Brand</title>
 </head>
 <body>
-    
+    <section id="header">
+        <div class="header">
+            <div class="container">
+                <div class="headerM">
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+
 </body>
 </html>
