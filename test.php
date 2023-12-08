8d04ed9c7d13f0acfc88fae5e43254b8ffa72b16
tareqmonuwar <tareqmonuwar4@gmail.com>
2023-12-08 23:16:29 +0600

    blog update css

--- test.php
+++ test.php
@@ -3,9 +3,8 @@ include_once ("assets/php/function.php");
 $DBN = "createbrand";
 $conne = new funtion_a($DBN);
 
-//$conne->database($DBN);
-
-// $conne->admin_t();
+// $conne->database($DBN);
+ // $conne->admin_t();
 
 
 
