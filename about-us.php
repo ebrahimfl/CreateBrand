28afc9be3213357f9b75aba069bb3d1691edf3e7
Redwan-Hussain-Shawon <shawonredwanhussain2@gmail.com>
2023-12-28 18:31:46 +0600

    about slider add

--- about-us.php
+++ about-us.php
@@ -15,21 +15,21 @@
             
         
             <div class="img">
-                <img src="https://media.istockphoto.com/id/914886270/photo/close-up-of-fresh-flame-grilled-burgers-displayed-in-a-row-at-food-market.jpg?s=2048x2048&w=is&k=20&c=zAHiTsjq8ua5TPa3ap01c-WRmiSChSXF_BvvckxGm8s=" alt="">
+                <img src="assets/images/slider/about-slider-1.svg" alt="" style='height:100%'>
             </div>
             <div class="img">
-                <img src="https://media.istockphoto.com/id/1470554127/photo/fried-chicken-sandwich-sliders.jpg?s=2048x2048&w=is&k=20&c=jrNmQpIBpFzKMwS-ZfJ_UOdOPkxXupBKbHKLoMCjg_k=" alt="">
+                <img src="assets/images/slider/about-slider-2.svg" alt="">
             </div>
             <div class="img">
-                <img src="https://media.istockphoto.com/id/1033776910/photo/woman-eating-mini-burgers-with-fast-food-with-french-fries-chips.jpg?s=2048x2048&w=is&k=20&c=NTq4jT_R_OSX5CgnjemodteesVjlmfs-sTopEz6KKdk=" alt="">
+                <img src="assets/images/slider/about-slider-3.svg" alt="">
             </div>
         </div>
         <div class="row">
             <div style='width:50%' class='text'>
                 <h2>The Web Design & Development Agency</h2>
-                <p>We are a Web Design and Development agency with the full package of a website building including Graphic Design, Facebook Marketing, SEO We are an experienced and talented team of passionate developers who lives and breathe in web design and development.</p>
+                <p>ক্রিয়েট অফ ব্রান্ড  শুধুমাত্র একটা নাম নয়, এটা সত্তিকারের একটা ব্রান্ড। আমাদের আছে একটা প্রফেশনাল টিম। বিভিন্ন জায়গা থেকে বিভিন্ন কাজের উপর অভিজ্ঞ ও প্রোফেশনাল মেম্বার। আমারা সবাই মিলে আমাদের বিভিন্ন জ্ঞান গুলোকে একত্রে করে প্রভাব ফেলছি  ধীরে ধীরে  এই অনলাইনের জগৎ এ। এই আশাতেই টিম তৈরি করেছি আমরা যেন এই অনলাইন জগতের নতুন ভাবে মানুষের সেবা দিতে পারি। আমাদের কাছে আসলে আপনি আশা করি, সব ধরনের সেবা আমারা দিতে পারব। আপনার যদি প্রয়োজন হয় ইথিকেল হ্যাকার থেকে শুরু করে যে কোন রকমের ওয়েব সাইটের,  যে কোন রকম বাগ ফিক্স, ডিজাইন অথবা ডেভলাপমেন্ট। অথবা যদি SEO, ডিজিটাল মার্কেটিং এবং অন্য যে কোন রকম সেবা। আপনি আমাদের ক্রিয়েট অফ ব্রান্ড টিমার সাথে যোগাযোগ করুন আমরা খুবই সল্প খরচে অল্প সময়ের মধ্যে আপনার সমস্যার সমাধান দিব ইনশাআল্লাহ। আমারা কখনই আমাদের কাজ দ্বারা আপনাদের অসন্তুষ্ট করব না, আপনাদের সন্তষ্টি আমাদের গর্ব ।</p>
 
-                <p>We have many clients around the world like United States, Australia, United Kingdom, Canada, France, Germany, and many more</p>
+              
                 <div style='margin-top: 25px;'>
                     <a href="contact-us.php" class='btn_normal'>Contact Us</a>
                 </div>
