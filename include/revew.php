a97348516bba99fcf7fb7eb34b1cd4655e2aeec0
EbrahiM <ebrahim49119782@gmail.com>
2024-01-07 15:11:51 +0600

    cms

---
+++ include/revew.php
@@ -0,0 +1,51 @@
+<link rel="stylesheet" href="assets/css/service-details.css">
+<div class="container" id="service-details">
+<div class="review">
+            <h2>This Service Review </h2>
+            <div class="row">
+                <div class="card">
+                    <div class="img">
+                        <img src="assets/images/portrait-white-man-isolated_53876-40306.avif" alt="">
+                    </div>
+                    <div class="review-star-icons">
+                        <img src="assets/icons/star-review-1.png" alt="">
+                        <img src="assets/icons/star-review-1.png" alt="">
+                        <img src="assets/icons/star-review-1.png" alt="">
+                        <img src="assets/icons/star-review-1.png" alt="">
+                        <img src="assets/icons/star-review-0.png" alt="">
+                    </div>
+                    <h3>Super customer service!</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam, adipisci alias? Rem harum a architecto animi tempora ducimus eos officiis incidunt fugiat quidem atque ipsum, unde, inventore nihil porro eius.</p>
+                </div>
+                <div class="card">
+                    <div class="img">
+                        <img src="assets/images/portrait-white-man-isolated_53876-40306.avif" alt="">
+                    </div>
+                    <div class="review-star-icons">
+                        <img src="assets/icons/star-review-1.png" alt="">
+                        <img src="assets/icons/star-review-1.png" alt="">
+                        <img src="assets/icons/star-review-1.png" alt="">
+                        <img src="assets/icons/star-review-1.png" alt="">
+                        <img src="assets/icons/star-review-0.png" alt="">
+                    </div>
+                    <h3>Super customer service!</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam, adipisci alias? Rem harum a architecto animi tempora ducimus eos officiis incidunt fugiat quidem atque ipsum, unde, inventore nihil porro eius.</p>
+                </div>
+                <div class="card">
+                    <div class="img">
+                        <img src="assets/images/portrait-white-man-isolated_53876-40306.avif" alt="">
+                    </div>
+                    <div class="review-star-icons">
+                        <img src="assets/icons/star-review-1.png" alt="">
+                        <img src="assets/icons/star-review-1.png" alt="">
+                        <img src="assets/icons/star-review-1.png" alt="">
+                        <img src="assets/icons/star-review-1.png" alt="">
+                        <img src="assets/icons/star-review-0.png" alt="">
+                    </div>
+                    <h3>Super customer service!</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam, adipisci alias? Rem harum a architecto animi tempora ducimus eos officiis incidunt fugiat quidem atque ipsum, unde, inventore nihil porro eius.</p>
+                </div>
+            </div>
+
+        </div>
+</div>
---
+++ include/team_slider.php
@@ -0,0 +1,50 @@
+
+<link rel="stylesheet" href="assets/css/service-details.css">
+
+<div class="container">
+    <div class="our-team" margin-top='30px'>
+            <h2 style="margin-top:80px" class="an fade-right" data-an="fade-right">Our Team</h2>
+            <div class="rows">
+                <div class="card an fade-left" data-team="Websaite Development" data-an="fade-left">
+                    <img src="assets/images/team/521188847.png" alt="" class="card-img">
+                    <div class="text">
+                        <h3>wewewe</h3>
+                        <p>Websaite Development</p>
+                        <a href="#"><img src="assets/icons/view-icons.png">View</a>
+                    </div>
+                </div>
+                <div class="card an fade-left" data-team="Digital Marketing" data-an="fade-left">
+                    <img src="assets/images/team/1813586188.png" alt="" class="card-img">
+                    <div class="text">
+                        <h3>Rahim</h3>
+                        <p>Digital Marketing</p>
+                        <a href="#"><img src="assets/icons/view-icons.png">View</a>
+                    </div>
+                </div>
+                <div class="card an fade-left" data-team="Websaite Development" data-an="fade-left">
+                    <img src="assets/images/team/594399166.jpg" alt="" class="card-img">
+                    <div class="text">
+                        <h3>Ebrahim</h3>
+                        <p>Websaite Development</p>
+                        <a href="#"><img src="assets/icons/view-icons.png">View</a>
+                    </div>
+                </div>
+                <div class="card an fade-left" data-team="Full Stack Developer" data-an="fade-left">
+                    <img src="assets/images/team/1346314550.jpg" alt="" class="card-img">
+                    <div class="text">
+                        <h3>Redwan Hussain Shawon</h3>
+                        <p>Full Stack Developer</p>
+                        <a href="#"><img src="assets/icons/view-icons.png">View</a>
+                    </div>
+                </div>
+                <div class="card an fade-left" data-team="SEO" data-an="fade-left">
+                    <img src="assets/images/team/e.jpg" alt="" class="card-img">
+                    <div class="text">
+                        <h3>Ebrahim</h3>
+                        <p>SEO</p>
+                        <a href="#"><img src="assets/icons/view-icons.png">View</a>
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
