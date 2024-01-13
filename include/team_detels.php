7d88b0279284c5b463e723945967467d23a82856
EbrahiM <ebrahim49119782@gmail.com>
2024-01-13 16:44:50 +0600

    team vew dy

--- include/team_detels.php
+++ include/team_detels.php
@@ -9,6 +9,7 @@ if (!$id) {
  $function = new fun(); 
  $row =  $function->show_col("admin",$id);
  $check = mysqli_num_rows($row);
+ 
 
  if ($check === 1) {
         while ($data = $row->fetch_assoc()) {
@@ -17,13 +18,12 @@ if (!$id) {
             $ad_name = $data['ad_name'];
             $ad_catagory = $data['ad_catagory'];
             $location = $data['location'];            
-            $dsc = $data['dsc'];
-            $ad_catagory = $data['ad_catagory'];
-            $ad_catagory = $data['ad_catagory'];
+            $dsc = $data['dsc'];            
             $datea=date_create($data['data']);
             $join_date = date_format($datea,"d M Y");
         }    
     }  
+    
 
 ?>
 <div id="team_details">
@@ -46,47 +46,82 @@ if (!$id) {
                     </div>
                 </div>
                 <p><?php echo $dsc ?> </p>
-                <div class="skill_keywords">
-                    <button>Real Estate Venture Capital</button>
-                    <button>Market Segmentaion </button>
-                    <button>Market Sizing </button>
-                    <button>Business Research </button>
-                    <button>Financial Modeling </button>
-                    <button>Market Segmentaion </button>
-                    <button>Market Segmentaion </button>
-                    <button>Financial Modeling </button>
-                    <button>Market Sizing </button>
-                    <button>Real Estate Venture Capital</button>
-                </div>
-                <div class="warrper">
-                    <div class='warrper_2'>
-                        <h4>Project Highlights</h4>
-                        <div class='warrper_row'>
-                            <div>
-                                <h5>Partner</h5>
-                                <p>Strategia Partners</p>
-                            </div>
-                            <div>
-                                <h5>Partner</h5>
-                                <p>Strategia Partners</p>
-                            </div>
-                            <div>
-                                <h5>Partner</h5>
-                                <p>Strategia Partners</p>
-                            </div>
-                        </div>
-                    </div>
 
+
+                <!-- skill -->
+                <div class="skill_keywords">
+                    <h4 style="display: block; width:100%">Skill</h4> 
+                    <?php 
+                    $skill = $function->skill("admin_skill", $ad_id);
+                        
+                            while ($skilla=$skill->fetch_assoc()) {
+                                // skill show
+                                $skillShow = $skilla['skill'];
+                    ?>
+                        <button><?php echo $skillShow; ?></button>
+                    <?php
+                            }
+                        
+                    ?>
+                    
                 </div>
+
+
+
+
+                <!-- project -->                
+            <section id="blog">                
+                <div class="rows">
+                <h4>Project</h4>
+                    <div class="content"> 
+                                                
+                        <?php 
+                                $project = $function->skill("admin_project", $ad_id);
+                            
+                                while ($skilla=$project->fetch_assoc()) {
+                                    // skill show
+                                    $pro_id = $skilla['id'];
+                                    $pro_project = $skilla['project'];
+                                    $pro_title = $skilla['title'];
+                                    $pro_des = $skilla['des'];
+                                    $pro_img = $skilla['img'];
+                            ?>
+                                <a href="" class="blog_link an" data-an='fade'>
+                                    <div class="card1">
+                                        <div class="img">
+                                            <img src="assets/images/project/<?php echo $pro_img  ?>" alt="">
+                                        </div>
+                                        <div class="blog_text">
+                                            <h3><?php echo substr($pro_title,0,15)."..." ?></h3>
+                                            <p><?php echo substr($pro_des,0,50)."..." ?></p>
+                                            
+                                        </div>
+                                    </div>
+                                </a>  
+                            <?php
+                                }
+                            
+                            ?>                      
+                                                  
+                    </div>
+                </div>
+            </section>
+        
+      
             <div class="work_experience">
                 <h3>Work Experience</h3>
+            </div>
+
+            <div class="work_experience">
                 <div class="work_content">
                     <div>
-                        <h4>Partner</h4>
+                        <h3>About</h3>
                     </div>
-                    <p class='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro repudiandae ullam molestias laudantium ab quasi fugiat harum. Sit debitis optio illo. Doloremque, facere! Quam recusandae modi nihil quaerat, dolore obcaecati unde aliquid tempora incidunt odit sunt excepturi dignissimos ea aut, minima alias iste atque placeat! Tempore cum voluptatum nemo perspiciatis sint corporis officiis quia optio nostrum. Vel veritatis enim vero odio repudiandae vitae quaerat, voluptate nulla quam fuga saepe harum placeat. Qui aut velit eos corrupti facilis illum nobis adipisci delectus incidunt praesentium. Enim, atque! Nulla vero facere doloremque. Necessitatibus nobis at quis id a, molestiae quasi eveniet beatae explicabo culpa rem totam illum blanditiis dolores illo itaque repudiandae perspiciatis ipsum facilis minus molestias? Nobis voluptas eius ipsum. Explicabo cupiditate eos nobis optio suscipit non fuga dolores dignissimos minima at commodi eius, corrupti aut deserunt necessitatibus rerum delectus saepe tempora dicta velit iure quidem recusandae ut? Corporis, culpa ut praesentium similique eveniet illum et ad quibusdam voluptate molestias sapiente numquam quos non quidem dolore obcaecati, consectetur ipsa blanditiis quo sequi vero ratione assumenda rerum tempora. Amet, minus et. Quibusdam deleniti ipsam possimus debitis, tenetur quas fugiat, quia non suscipit sed nulla dolores obcaecati veritatis? Tempora molestias id vero iste magnam.</p>
+                    <p class='description'><?php echo $dsc  ?></p>
                 </div>
             </div>
+
+            
                 
             </div>
         </div>
@@ -94,3 +129,4 @@ if (!$id) {
 </div>
 
 <?php include_once("include/team_slider.php"); ?>
+
