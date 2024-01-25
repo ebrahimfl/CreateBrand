89bdb253497c80e474968669a6699a092b98cd45
Redwan-Hussain-Shawon <shawonredwanhussain2@gmail.com>
2024-01-25 22:56:04 +0600

    home page responsive

--- include/headerM.php
+++ include/headerM.php
@@ -104,9 +104,13 @@
                             </ul>
                         </div>
                     </li> -->
+        <?php 
+            $result = $function->show_col('user',$_COOKIE[md5('name')]);
+            $data = $result->fetch_assoc();
+        ?>
                     <li><a href="blog.php">Blog</a></li>
                     <?php if (isset($_COOKIE[md5('name')])) {  ?>
-                        <li><a href="user_profile.php" style="padding: 0;"><img src="assets/images/e.jpg" alt="" srcset="" width="50px" height="50px" style="border-radius: 25px;"></a></li>
+                        <li><a href="user_profile.php" style="padding: 0;"><img src="assets/images/user/<?php echo $data['img'] ?>" alt="" srcset="" width="50px" height="50px" style="border-radius: 25px;"></a></li>
                     <?php } ?>
                 </ul>
                 <div class="menu-icon">
